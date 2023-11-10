9631fd62a2ebbda5a64daf2662b949470d85dcbd
egimbaeva22 <alinaegimbaeva2006@gmail.com>
2023-11-11 00:43:57 +0600

    dz2

---
+++ app.js
@@ -0,0 +1,26 @@
+var month = prompt('enter month -').toLowerCase()
+
+if( month === 'март'|| month === 'апрель' || month === 'май' ){
+    alert('весна')
+}else if ( month === 'июнь'|| month === 'июль' || month === 'август' ){
+    alert('лето')
+}else if ( month === 'сентябрь'|| month === 'октябрь' || month === 'ноябрь' ){
+    alert('осень')
+}else if ( month === 'декабрь'|| month === 'январь' || month === 'февраль' ){
+    alert('зима')
+}else{
+    alert('введите корректные данные')
+}
+
+var array = [[[[[[[[[[[2]]]]]]]]]]];
+var result = array[0][0][0][0][0][0][0][0][0][0][0];
+console.log(result);
+
+var num1 = Number(prompt('num1'))
+var num2 = Number(prompt('num2'))
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1/num2)
+console.log(num1*num2)
+
+
